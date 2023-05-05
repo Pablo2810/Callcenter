0c43aa682ce77b39a90aba5dd9c34382e0f81ef4
Pablo2810 <pablorocha.2810@gmail.com>
2023-05-05 20:26:40 -0300

    Ejercicio 2 del TP2 JUNIT (callcenter)

---
+++ src/ar/edu/unlam/pb2/Contacto.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.pb2;
+
+public class Contacto {
+	private String nombreCompleto;
+	private Integer celular;
+	private String email;
+	private String direccion;
+	private Integer codPostal;
+	private String localidad;
+	private Provincia provincia;
+	private Boolean esCliente;
+	private Boolean llamarNuevamente;
+	
+	
+}
